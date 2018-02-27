c08cd889ed756ba2f0863455371a136c31f83251
Tomasz Barański <tomasz.baranski@collective-sense.com>
2018-02-27 12:34:28 +0100

    ParseMsg parses RefreshCatalog reply

--- src/main/kotlin/co/llective/hyena/util/ParseMsg.kt
+++ src/main/kotlin/co/llective/hyena/util/ParseMsg.kt
@@ -1,8 +1,6 @@
 package co.llective.hyena.util
 
-import co.llective.hyena.api.ListColumnsReply
-import co.llective.hyena.api.MessageDecoder
-import co.llective.hyena.api.Reply
+import co.llective.hyena.api.*
 import java.io.File
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
@@ -11,9 +9,24 @@ fun printUsage() {
     println("Usage: java -jar hyena-api-parsemsg.jar <filename>")
 }
 
+fun reduceColumns(columns: List<Column>) =
+    if (columns.isEmpty()) {
+        ""
+    } else {
+        columns.map { column -> column.toString() }.reduce { x, y -> x + ", " + y }
+    }
+
 fun printReply(reply: Reply) {
     when (reply) {
         is ListColumnsReply -> println(reply)
+        is CatalogReply -> {
+            println("CatalogReply(columns=[" +
+                    reduceColumns(reply.result.columns)
+                    + "], partitions=#" + reply.result.availablePartitions.size + ")")
+        }
+        is AddColumnReply -> {
+
+        }
     }
 }
 
